c9fdd1d384d07ff3cfacdc12d1b5f5ea4df5e6f2
AgathePons <ponsagathe37@gmail.com>
2022-02-17 14:08:20 +0100

    add test request get with query

--- app/controllers/controller.js
+++ app/controllers/controller.js
@@ -3,12 +3,8 @@ const {cadex} = require('../services/cadex');
 
 const controller = {
   getCadex(req, res) {
-    //!
-    console.log(req.query);
     const cadexObject = cadex.generate();
     const copy = {...cadexObject, ...req.query};
-    //!
-    console.log(copy);
     return res.json(copy.glue()); // on doit return pour utiliser le résultat dans le test
   },
 };
