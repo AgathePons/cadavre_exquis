4a019dd6928ff5147bb655cc04286b5396038db3
AgathePons <ponsagathe37@gmail.com>
2022-05-23 11:03:19 +0200

    modify seeding

--- data/import_data.sql
+++ data/import_data.sql
@@ -1,47 +1,69 @@
 BEGIN;
+
+TRUNCATE
+  name,
+  adjective,
+  verb,
+  complement,
+  sentence
+RESTART IDENTITY;
+
 INSERT INTO
   name (label)
 VALUES
-  ('un cheval'),
-  ('la mairie de Neuilly-sur-Seine'),
-  ('une huître'),
-  ('Julie Andrieu'),
-  ('Jacky et sa Subaru Impreza'),
-  ('la gendarmerie hollandaise'),
-  ('un chauve'),
-  ('Simon'),
-  ('2 chatons');
+  ('Un cheval, ou peut-être une licorne'),
+  ('Le Dalaï-Lama en personne'),
+  ('Une huître'),
+  ('Mickey Mouse'),
+  ('Mon petit doigt (celui de la main gauche bien sûr)'),
+  ('La fusée Ariane (pas le fil, la fusée)'),
+  ('Tintin & Milou'),
+  ('Le Grand Architecte'),
+  ('Jérôme Bosch'),
+  ('Le monstre en spaghetti volant'),
+  ('Cthulhu');
 INSERT INTO
   adjective (label)
 VALUES
-  ('bien cuit'),
-  ('blond'),
-  ('guilleret'),
-  ('ankylosé'),
-  ('blafard'),
+  ('plaqué or'),
+  ('décoloré blond-platine'),
+  ('guilleret à l''extérieur, mais mort à l''intérieur'),
+  ('en jupe à froufrou'),
+  ('blafard sous l''éclairage des néons'),
   ('rasé de près'),
-  ('amputé d''un doigt');
+  ('la tête dans les nuages'),
+  ('beau comme un camion'),
+  ('dans sa plus belle tenue de go-go dancing'),
+  ('en équilibre sur la corde raide'),
+  ('K''yarnak ch'' tharanak Shub-Niggurath');
+INSERT INTO
+  verb (label)
+VALUES
+  ('rêve de'),
+  ('consulte lors d''un referendum'),
+  ('énerve franchement la communauté de'),
+  ('cuisine à l''étouffée'),
+  ('remet en question'),
+  ('s''immole pour protester contre'),
+  ('s''enduit de'),
+  ('instaure en grande pompe'),
+  ('déguste pour le goûter'),
+  ('descend de son perchoir muni de'),
+  ('ngAzathoth');
 INSERT INTO
   complement (label)
 VALUES
   ('un seau en plastique'),
-  ('la consommation de café'),
-  ('Yann'),
-  ('3 roues de voiture'),
-  ('2 mains gauches'),
-  ('un skieur débutant'),
-  ('la Mer Noire');
-INSERT INTO
-  verb (label)
-VALUES
-  ('consulte'),
-  ('franchit'),
-  ('cuisine'),
-  ('remet en question'),
-  ('s''immole pour protester contre'),
-  ('enduit de confiture'),
-  ('instaure'),
-  ('déguste');
+  ('un bon café bien serré'),
+  ('le Paradis Pastafarien'),
+  ('trois roulettes de trotinette'),
+  ('la trop bonne confiture de mûre'),
+  ('un taxidermiste en puissance'),
+  ('la Mer Noire'),
+  ('un machin bien moche et bien nul'),
+  ('ses grands morts'),
+  ('son Bescherelle'),
+  ('R''lyeh wgah''nagl');
 INSERT INTO
   sentence (
     noun_id,
